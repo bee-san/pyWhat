2c17519786253301c8661ad02836df5599a940be
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2021-05-31 12:06:33 +0300

    Update tests and docs

--- pywhat/what.py
+++ pywhat/what.py
@@ -21,9 +21,9 @@ def print_tags(ctx, opts, value):
 )
 @click.argument("text_input", required=True)
 @click.option("--tags", is_flag=True, expose_value=False, callback=print_tags, help="Show available tags and exit.")
-@click.option("--rarity")
-@click.option("--include_tags")
-@click.option("--exclude_tags")
+@click.option("--rarity", help="Filter by rarity.")
+@click.option("--include_tags", help="Only print entries with included tags.")
+@click.option("--exclude_tags", help="Exclude tags.")
 def main(text_input, rarity, include_tags, exclude_tags):
     """
     What - Identify what something is.\n
@@ -32,6 +32,14 @@ def main(text_input, rarity, include_tags, exclude_tags):
 
     https://github.com/bee-san\n
 
+    Filtration:\n
+        --rarity min:max\n
+            Only print entries with rarity in range [min,max]. min and max can be omitted.\n
+        --include_tags list\n
+            Only include entries containing at least one tag in a list. List is a comma separated list.\n
+        --include_tags list\n
+            Exclude specified tags. List is a comma separated list.\n
+
     Examples:
 
         * what "HTB{this is a flag}"
@@ -39,6 +47,8 @@ def main(text_input, rarity, include_tags, exclude_tags):
         * what "0x52908400098527886E0F7030069857D2E4169EE7"
 
         * what -- 52.6169586, -1.9779857
+
+        * what --rarity 0.6: myEmail@host.org
 
     Your text must either be in quotation marks, or use the POSIX standard of "--" to mean "anything after -- is textual input".
 
@@ -53,10 +63,14 @@ def main(text_input, rarity, include_tags, exclude_tags):
         if len(rarities) != 2:
             print("Invalid rarity range format ('min:max' expected)")
             sys.exit(1)
-        if not rarities[0].isspace() and rarities[0]:
-            min_rarity = float(rarities[0])
-        if not rarities[1].isspace() and rarities[1]:
-            max_rarity = float(rarities[1])
+        try:
+            if not rarities[0].isspace() and rarities[0]:
+                min_rarity = float(rarities[0])
+            if not rarities[1].isspace() and rarities[1]:
+                max_rarity = float(rarities[1])
+        except ValueError:
+            print("Invalid rarity argument (float expected)")
+            sys.exit(1)
 
     if include_tags is not None:
         included_tags = list(map(str.strip, include_tags.split(',')))
--- tests/test_identifier.py
+++ tests/test_identifier.py
@@ -6,3 +6,27 @@ def test_identifier_works():
     out = r.identify("DANHz6EQVoWyZ9rER56DwTXHWUxfkv9k2o")
     assert "Dogecoin (DOGE) Wallet Address" in out["Regexes"][0]["Regex Pattern"]["Name"]
 
+
+def test_rarity_filtration():
+    r = identifier.Identifier()
+    out = r.identify("someguy@gmail.com", min_rarity=0.6)
+    assert len(out["Regexes"]) == 0
+
+
+def test_rarity_filtration2():
+    r = identifier.Identifier()
+    out = r.identify("ScOAntcCa78", max_rarity=0.1)
+    assert len(out["Regexes"]) == 0
+
+
+def test_tag_filtration():
+    r = identifier.Identifier()
+    out = r.identify("fixtures/file", included_tags=["Cyber Security"])
+    for regex in out["Regexes"]:
+        assert "Cyber Security" in regex["Regex Pattern"]["Tags"]
+
+
+def test_tag_filtration2():
+    r = identifier.Identifier()
+    out = r.identify("+91 (385) 985 2821", excluded_tags=["Identifiers", "Credentials"])
+    assert len(out["Regexes"]) == 0
