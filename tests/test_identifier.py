363607eb8974aa859f46fc55b389c6eca3f9c6bc
amadejpapez <amadej.papez@icloud.com>
2021-06-01 10:41:50 +0200

    Add tests if API gets file path as an input

--- tests/test_identifier.py
+++ tests/test_identifier.py
@@ -6,3 +6,12 @@ def test_identifier_works():
     out = r.identify("DANHz6EQVoWyZ9rER56DwTXHWUxfkv9k2o")
     assert "Dogecoin (DOGE) Wallet Address" in out["Regexes"]["None"][0]["Regex Pattern"]["Name"]
 
+def test_identifier_works2():
+    r = identifier.Identifier()
+    out = r.identify("fixtures/file")
+    assert "Ethereum (ETH) Wallet Address" in out["Regexes"]["file"][0]["Regex Pattern"]["Name"]
+
+def test_identifier_works3():
+    r = identifier.Identifier()
+    out = r.identify("fixtures/file")
+    assert "Dogecoin (DOGE) Wallet Address" in out["Regexes"]["file"][1]["Regex Pattern"]["Name"]
