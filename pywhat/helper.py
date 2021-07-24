f551f4afac2453da93659915216cc4cbb3be62ec
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2021-07-24 09:53:15 +0300

    Fix json error

--- pywhat/helper.py
+++ pywhat/helper.py
@@ -29,7 +29,7 @@ class InvalidTag(Exception):
 def load_regexes() -> list:
     path = "Data/regex.json"
     fullpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    with open(fullpath, "r", encoding="utf-8") as myfile:
+    with open(fullpath, "rb") as myfile:
         regexes = json.load(myfile)
     for regex in regexes:
         regex["Boundaryless Regex"] = re.sub(
