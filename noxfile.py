b4567ff2e1de194b4ee6a79298c95502c3cf7b93
SkeletalDemise <29117662+SkeletalDemise@users.noreply.github.com>
2021-10-04 18:47:38 -0700

    Simplify nox file and add pytest-mypy

--- noxfile.py
+++ noxfile.py
@@ -4,8 +4,7 @@ from typing import Any
 import nox
 from nox.sessions import Session
 
-package = "hypermodern_python"
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "tests"
 locations = "src", "tests", "noxfile.py", "docs/conf.py"
 
 
@@ -35,40 +34,10 @@ def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> Non
 
 
 @nox.session
-def black(session: Session) -> None:
-    """Run black code formatter."""
-    args = session.posargs or locations
-    install_with_constraints(session, "black")
-    session.run("black", *args)
-
-
-@nox.session
-def lint(session: Session) -> None:
-    pass
-
-
-@nox.session
 def tests(session: Session) -> None:
     """Run the test suite."""
     session.run("poetry", "install", "--no-dev", external=True)
-    install_with_constraints(session, "pytest", "pytest-black", "pytest-isort")
-    session.run("pytest")
-
-
-@nox.session
-def typeguard(session: Session) -> None:
-    """Runtime type checking using Typeguard."""
-    args = session.posargs or ["-m", "not e2e"]
-    session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(
-        session, "pytest", "pytest-mock", "typeguard", "pytest-black", "pytest-isort"
+        session, "pytest", "pytest-black", "pytest-isort", "pytest-mypy"
     )
-    session.run("pytest", f"--typeguard-packages={package}", *args)
-
-
-@nox.session
-def coverage(session: Session) -> None:
-    """Upload coverage data."""
-    install_with_constraints(session, "coverage[toml]", "codecov")
-    session.run("coverage", "xml", "--fail-under=50")
-    session.run("codecov", *session.posargs)
+    session.run("pytest")
